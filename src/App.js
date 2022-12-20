9020d1f0c6df742a300b3d87991c386de46de3dd
jeffpatriota <jefferson.patriota@outlook.com>
2022-12-20 20:05:50 -0300

    Requisicoes HTTP e Rotas

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import './style.css';
+
+///https://sujeitoprogramador.com/rn-api/?api=posts
+
+function App() {
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(() => {
+
+    function loadApi() {
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+
+      fetch(url)
+        .then((result) => result.json())
+        .then((json) => {
+          console.log(json);
+          setNutri(json);
+        })
+    }
+
+    loadApi();
+
+  }, [])
+
+  return (
+    <div className="container" >
+      <header>
+        <strong>React Nutri</strong>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
