d4b6ce374a23bc04b0f7ceb5c7c49c602933bd23
jeffpatriota <jefferson.patriota@outlook.com>
2022-12-22 17:29:32 -0300

    Requisicoes HTTP e Rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
-///https://sujeitoprogramador.com/rn-api/?api=posts
+////https://sujeitoprogramador.com/rn-api/?api=posts
+
+
 
 function App() {
   const [nutri, setNutri] = useState([]);
@@ -9,26 +11,39 @@ function App() {
   useEffect(() => {
 
     function loadApi() {
-      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
 
       fetch(url)
-        .then((result) => result.json())
+        .then((r) => r.json())
         .then((json) => {
           console.log(json);
           setNutri(json);
         })
+
     }
 
     loadApi();
-
-  }, [])
+  }, []);
 
   return (
-    <div className="container" >
+    <div className="container">
       <header>
         <strong>React Nutri</strong>
       </header>
 
+      {nutri.map((item) => {
+        return (
+          <article key={item.id} className="post">
+            <strong className="titulo">{item.titulo}</strong>
+
+            <img src={item.capa} alt={item.titulo} className="capa" />
+            <p className="subtitulo">
+              {item.subtitulo}
+            </p>
+            <a className="botao">Acessar</a>
+          </article>
+        )
+      })}
     </div>
   );
 }
